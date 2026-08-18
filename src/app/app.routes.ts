b48f5f0cb391871822c6bb9125e6d1b3000540c8
mehrsa amini <1273130367@C1813735>
2026-08-18 09:56:05 +0330

    EditedAppRout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -53,11 +53,10 @@ export const routes: Routes = [
       },
     ],
   },
-  { path: 'task/list', redirectTo: 'project/list', pathMatch: 'full', canActivate: [authGuard] },
+  { path: 'task/list', redirectTo: 'project/list', pathMatch: 'full' },
   {
     path: 'task/list/:projectId',
     redirectTo: 'project/:projectId/backlog',
     pathMatch: 'full',
-    canActivate: [authGuard],
   },
 ];
